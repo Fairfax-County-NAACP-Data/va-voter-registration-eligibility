891803fb84c6730820d793a2aeb6e58424941d5e
sowdm <matthew.sowd@gmail.com>
2026-08-09 07:46:57 -0600

    Updated text

--- voter_guide_app.py
+++ voter_guide_app.py
@@ -29,8 +29,10 @@ if pd.Timestamp.now() < pd.Timestamp('2026-09-05 13:00:00'):
 pg = st.navigation([st.Page("1_intro.py", title='FAQ'), st.Page('2_eligibility.py', title='Eligibility')], position='top')
 pg.run()
 
-st.info('This site contains information published by the [Virginia Department of Elections](https://www.elections.virginia.gov/registration/) except for the '+
+st.info('This site was created by Fairfax County NAACP. The information published by this site '
+        'was provided by the [Virginia Department of Elections](https://www.elections.virginia.gov/registration/) except for the '+
         "guidance for how to fill out the felony questions on the voter registration form, which was provide by the "
-        "[Fairfax County Registrar's Office](https://www.fairfaxcounty.gov/elections/registration).\n\n"
+        "[Fairfax County Registrar's Office](https://www.fairfaxcounty.gov/elections/registration) "
+        "in private communications.\n\n"
         'For questions about voter registration, please contact the [Fairfax County Registrar](https://www.fairfaxcounty.gov/elections/registration). \n\n'+
         'For questions/comments/issues about this site, please email [us](mailto:voting_guide@pm.me).')
